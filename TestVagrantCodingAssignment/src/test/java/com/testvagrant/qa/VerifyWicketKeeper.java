f81dc68a345728a1b952cd61f40eb22659ac78d5
sarathselvi <sarath13ecb25@gmail.com>
2022-08-11 16:25:11 +0530

    Add - Test to verify if wicket-keeper present

---
+++ TestVagrantCodingAssignment/src/test/java/com/testvagrant/qa/VerifyWicketKeeper.java
@@ -0,0 +1,23 @@
+package com.testvagrant.qa;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.testvagrant.constants.FrameworkConstants;
+
+public class VerifyWicketKeeper {
+
+	@Test
+	public void verifyWicketKeeper() throws IOException {
+
+		String response = new String(Files.readAllBytes(Paths.get(FrameworkConstants.getJsonresponse())));
+
+		Assert.assertEquals(response.contains("Wicket-keeper"), false);
+
+	}
+
+}
